9dff2433b67ac8002b8496d2127dc61db158eef2
Alessandro Koterle <alessandro.koterle@gmail.com>
2017-10-25 21:54:25 +0200

    initial commit

---
+++ backend/src/main/kotlin/com/akoterle/springbootreact/SpringBootReactApplication.kt
@@ -0,0 +1,11 @@
+package com.akoterle.springbootreact
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringBootReactApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBootReactApplication::class.java, *args)
+}
